bc1a439b56288d6f290744ed58ef75f15898ed96
unknown <hussainrizvi486@gmail.com>
2024-01-29 17:11:24 +0500

    -

--- apps/accounts/api/register.py
+++ apps/accounts/api/register.py
@@ -3,29 +3,33 @@ from apps.accounts.models import User
 from django.core.validators import validate_email
 from django.http import HttpResponseBadRequest
 from rest_framework.response import Response
+from rest_framework import status
 
 
 class RegisterUser(APIView):
     def post(self, request):
         user_object = request.data
-        # user_object = self.validate_user_object(user_object)
+        user_object = self.validate_user_object(user_object)
 
-        user = User.objects.create(
-            first_name=user_object.get("first_name"),
-            last_name=user_object.get("last_name"),
-            username=user_object.get("username"),
-            password=user_object.get("password"),
-            email=user_object.get("email"),
-            phone_number=user_object.get("phone_number"),
-        )
+        if self.validate_email(user_object.get("email")):
+            return Response(data="Email is not vaild", status=status.HTTP_403_FORBIDDEN)
 
-        user.save()
-        from apps.store.models.customer import Customer
+        # user = User.objects.create(
+        #     first_name=user_object.get("first_name"),
+        #     last_name=user_object.get("last_name"),
+        #     username=user_object.get("username"),
+        #     password=user_object.get("password"),
+        #     email=user_object.get("email"),
+        #     phone_number=user_object.get("phone_number"),
+        # )
 
-        if user:
-            customer = Customer.objects.create(
-                user=user, customer_name=f"{user.first_name} {user.last_name}"
-            )
+        # user.save()
+        # from apps.store.models.customer import Customer
+
+        # if user:
+        #     Customer.objects.create(
+        #         user=user, customer_name=f"{user.first_name} {user.last_name}"
+        #     )
         return Response({"message": "user registered"}, status=200)
 
     def validate_user_object(self, user_object: dict):
@@ -38,15 +42,12 @@ class RegisterUser(APIView):
             "password",
         ]
 
-        for key in user_object.items():
-            if key not in required_fields:
-                raise Exception(f"{key} is missing")
+        # for key in user_object.items():
+        #     if key not in required_fields:
+        #         return Response(data=f"{key} is missing")
 
         if not self.validate_email(user_object.get("email")):
-            raise Exception("email is not vaild")
-
-        if len(user_object.get("phone")) < 8:
-            raise Exception("Password is to short")
+            return Response(data="email is not vaild")
 
         return user_object
 
@@ -56,6 +57,6 @@ class RegisterUser(APIView):
         user = User.objects.filter(email=email)
 
         if user:
-            raise Exception("email already taken")
+            return True
 
-        return True
+        return False
